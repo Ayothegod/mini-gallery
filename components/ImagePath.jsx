be64d9b5031cce4e99e335ac231b032d1acce420
Ayo <heyayomideadebisi@gmail.com>
2023-04-13 01:20:53 +0100

    build: next/image error

--- components/ImagePath.jsx
+++ components/ImagePath.jsx
@@ -1,19 +1,18 @@
-import Image from "next/image";
-
-
 const ImagePath = (props) => {
   return (
     <div className=" p-3 shadow-lg bg-white rounded-md  ">
-      <Image
+      <img
         src={props.urls.small}
         alt={props.user.username}
+        loading="lazy"
         className="h-52 lg:h-72 w-full object-cover rounded-md"
       />
       <div className="text-sm flex mt-3 items-center justify-between">
-      <div className="flex items-center">
-          <Image
+        <div className="flex items-center">
+          <img
             src={props.user.profile_image.small}
             alt={props.user.username}
+            loading="lazy"
             className="h-8 w-8 md:h-10 md:w-10 object-cover rounded-full border border-slate-400"
           />
           <div>
@@ -22,7 +21,7 @@ const ImagePath = (props) => {
         </div>
         <div className="">
           <div className="flex items-center font-semibold underline cursor-pointer">
-            <p>@{props.user.first_name.split(" ",1)}</p>
+            <p>@{props.user.first_name.split(" ", 1)}</p>
           </div>
           <p className="text-[12px]">{props.likes} Likes</p>
         </div>
